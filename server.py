7d789062bb110c50843026c54b25ffcb54127c8d
catyeo18 <catherineyeo2018@gmail.com>
2023-02-20 14:37:46 -0500

    Update to multithreading

--- server.py
+++ server.py
@@ -1,52 +1,63 @@
-import socket 
-HOST = '127.0.0.1' 
-PORT = 6000
+import socket
+import mysql.connector
+import random
+from _thread import *
+import threading
 
-# key: username, value: dictionary of {recipient: messages}
-# accounts = {
-# 	"cat": {"haneul": [message1], "mohib": [message1, message2]}
-# }
+p_lock = threading.Lock()
+
+# accounts: {name: id}
 accounts = {}
 
+# key: ID, value: dictionary of {recipientID: messages}
+# messages = {
+#   sendUserID: {receiveUserID1: [message1], receiveUserID2: [message1, message2]}
+# }
+messages = {}
+
+# methods for different operations of wire protocol
+def create_account(username):
+  account_id = str(random.randint(0, 1000))
+
+  if username in accounts:
+    print("Username already exists.")
+    return
+
+  # generate ID
+  accounts[username] = account_id
+  messages[account_id] = {}
+  
+  data = "account_id: " + str(account_id) + "\n"
+  print(data)
+
+# multithreading
+def threaded(client):
+  while True:
+    data_list = []
+    # data received from client
+    data = client.recv(1024)
+    data_str = data.decode('UTF-8')
+    if not data:
+        print('Error: nothing received')
+        break
+    print(data_str + "\n")
+    
+    # Split data into each component
+    data_list = data_str.split('|')
+    opcode = data_list[0]
+    print("Opcode:" + str(opcode))
+    
+    # Wire protocol by opcode
+    if opcode == '1':
+      print("Param:" + data_list[1])
+      create_account(data_list[1])
+
+  # connection closed
+
+  client.close()
 
 
-def create_user(username):
-	if username in accounts:
-		print("Username already exists.")
-		return
-
-	accounts[username] = {}
-
-def server():
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-	serversocket.bind((HOST, PORT))
-
-	serversocket.listen()
-	clientsocket, addr = serversocket.accept()
-	print('Connected to by:', addr)
-
-	# Receive username
-	username = clientsocket.recv(128)
-	username = username.decode('ascii')
-	print("Received username is", username)
-	create_user(username)
-
-	# Receive recipient name
-	recipient = clientsocket.recv(128)
-	recipient = recipient.decode('ascii')
-	print("Received recipient is", recipient)
-	# TODO: check recipient name is valid, error otherwise
-	
-	# TODO: check if recipient is active and set up queue properly
-
-	# Receive message
-	msg = clientsocket.recv(128)
-	msg = msg.decode('ascii')
-	print("Received msg is", msg)
-	if recipient in accounts[username]:
-		accounts[username][recipient].append(msg)
-	else:
-		accounts[username][recipient] = [msg]
 
 
-server()
+
+  
