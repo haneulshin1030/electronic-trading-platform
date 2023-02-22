478bf018214203184bba30893a2995324e14f3b0
Haneul Shin <haneulshin1030@gmail.com>
2023-02-22 00:48:57 -0500

    Update client.py

--- regular/client.py
+++ regular/client.py
@@ -1,53 +1,75 @@
 import socket
+import sys
 
 from _thread import *
 import threading
 from threading import Thread
+import time
+
 
 # lock = threading.Lock()
+# sending_message = False
 
-sending_message = False
+event = threading.Event()
+event.set()
 
-def receive_messages(clientsocket):
-  global sending_message
+testing = False
+def print_test(text):
+  if testing:
+    print(text)
+
+def receive_server_messages(clientsocket):
+  # global sending_message
   while True:
-    if not sending_message:
-      data = clientsocket.recv(1024)
-      print('\nReceived from the server: ', str(data.decode('ascii')))
+    print_test("waiting 1a")
+    event.wait(timeout=0.1)
+    print_test("1b")
+    data = clientsocket.recv(1024)
+    print_test("1c")
+    event.clear()
+    print_test("1d")
+    # print_test('\nReceived from the server: ', str(data.decode('ascii')))
+    sys.stdout.flush()
+    print(str(data.decode('ascii')))
+    print_test("1e")
+    event.set()
+    print_test("1f")
   return
 
 def main():
   HOST = '127.0.0.1' 
-  PORT = 6025
+  PORT = 6043
 
   clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
   clientsocket.connect((HOST, PORT))
 
-  global sending_message
+  # global sending_message
 
-  # start_new_thread(receive_messages, (clientsocket,))
+  start_new_thread(receive_server_messages, (clientsocket,))
 
   # message to send to server
   while True:
-    ans = input("\nEnter your request: ")
+    print_test("waiting 2a")
+    event.wait()
+    print_test("2b")
+    time.sleep(0.001)
+    sys.stdout.flush()
+    print_test("2c")
+    event.clear()
+    print_test("2d")
+    ans = input("\n>>> ")
+    print_test("2e")
 
-    if ans == "":
-      ans2 = input("\nDo you want to continue (y/n):")
-      if ans2 == 'y':
-        continue
-      else:
-        return
-    else:
-      sending_message = True #lock..?
+    if ans != "":
+      # print(2)
       clientsocket.send(ans.encode('ascii'))
-      data = clientsocket.recv(1024)
-      print('Received from the server1: ', str(data.decode('ascii')))
-      sending_message = False
+    print_test("2f")
+    event.set()
+    print_test("2g")
 
       # print the received message
       # here it would be a reverse of sent message
   
-  print("Blah")
   # close the connection
   clientsocket.close()
 
