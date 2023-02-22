f7abd9373346e69abe649d278d0849a284f6e761
catyeo18 <catherineyeo2018@gmail.com>
2023-02-22 11:44:30 -0500

    Add server-client interaction unit tests

--- regular/unit_tests.py
+++ regular/unit_tests.py
@@ -1,7 +1,11 @@
 import unittest
+import time
+import socket
+import threading
+from _thread import *
 
 from server import list_accounts, login, send_message
-
+import server, client
 
 test_accounts = {
   "user1": 594,
@@ -50,6 +54,41 @@ class UnitTests(unittest.TestCase):
   def test_send_message2(self):
     send_message("user1", "user3", "message1to3b", {}, test_messages)
     self.assertEqual(test_messages["user3"]["user1"], ["\nFrom user1: message1to3a", "\nFrom user1: message1to3b"])
+
+  # To run these tests, have a server.py running in a separate shell
+  # An exception will pop up because of the way we're running a thread
+  # for testing purposes
+  def test_server_client(self):
+    HOST = '127.0.0.1'
+    PORT = 6065
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    client_socket.connect((HOST, PORT))
+
+    client.thread_running_event.set()
+    start_new_thread(client.receive_server_messages, (client_socket,))
+
+    # Create user
+    request = "create user1"
+    client_socket.send(request.encode('ascii'))
+    time.sleep(0.01)
+
+    # Test duplicate username, raise exception
+    request = "create user1"
+    self.assertRaises(Exception, client_socket.send(request.encode('ascii')))
+    time.sleep(0.01)
+
+    # Test login to account that doesn't exist
+    request = "login user2"
+    self.assertRaises(Exception, client_socket.send(request.encode('ascii')))
+    time.sleep(0.01)
+
+    # Test sending a message to account that doesn't exist
+    request = "send user2"
+    self.assertRaises(Exception, client_socket.send(request.encode('ascii')))
+    time.sleep(0.01)
+
+    client_socket.close()
+
 
 if __name__ == '__main__':
   print("Unit tests running!")
