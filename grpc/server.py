bc98735bcf0cb74598f6cf6d418b15f4727dd171
catyeo18 <catherineyeo2018@gmail.com>
2023-02-21 21:29:41 -0500

    grpc server setup

--- grpc/server.py
+++ grpc/server.py
@@ -1,4 +1,7 @@
 import grpc
+import threading
+from concurrent import futures
+
 import chatapp_pb2 as pb2
 import chatapp_pb2_grpc as pb2_grpc
 
@@ -42,3 +45,22 @@ class Server(pb2_grpc.ChatAppServicer):
       self.messages[receiver_name][sender_name] = [message]
 
     return pb2.Empty()
+
+
+def main():
+  HOST = '127.0.0.1'
+  PORT = 6060
+
+  # create gRPC server with max 4 threads at a time
+  server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+
+  # add server to gRPC
+  pb2_grpc.add_ChatAppServicer_to_server(Server(), server)
+  print("Socket is listening")
+
+  server.add_insecure_port('[::]:' + str(PORT))
+  server.start()
+  server.wait_for_termination()
+
+if __name__ == "__main__":
+  main()
