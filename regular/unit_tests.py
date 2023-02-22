bf3bf503574a4f0561beeb7404dfa42821f7be5a
catyeo18 <catherineyeo2018@gmail.com>
2023-02-22 00:01:23 -0500

    More unit tests

--- regular/unit_tests.py
+++ regular/unit_tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from server import list_accounts, login, send_message, send_undelivered_messages
+from server import list_accounts, login, send_message
 
 
 test_accounts = {
@@ -27,22 +27,30 @@ test_messages = {
 """Unit tests cover each method on our server side"""
 class UnitTests(unittest.TestCase):
   
-  # def test_create_accounts():
-    # did we not used to have a method for account creation?
-    # would be good to test to make sure duplicate usernames aren't allowed
-  
-  def test_list_accounts(self):
+  def test_list_accounts_empty(self):
     # Empty case = list all accounts
     self.assertEqual(list_accounts("", test_accounts), ["user1", "user2", "user3"])
     
-    # Regex cases
+  def test_list_accounts_regex(self):
+    # Regex cases to get subset of accounts
     self.assertEqual(list_accounts("^use", test_accounts), ["user1", "user2", "user3"])
     self.assertEqual(list_accounts("^user[13]$", test_accounts), ["user1", "user3"])
     self.assertEqual(list_accounts(".*1.*", test_accounts), ["user1"])
     self.assertEqual(list_accounts(".*[4567890].*", test_accounts), [])
 
+  def test_login(self):
+    self.assertEqual(login("", "user1", test_accounts), 594)
+    self.assertEqual(login("", "user2", test_accounts), 109)
+    self.assertEqual(login("", "user3", test_accounts), 837)
+  
+  def test_send_message1(self):
+    send_message("user1", "user2", "message1to2a", {}, test_messages)
+    self.assertEqual(test_messages["user2"]["user1"], ["message1to2a"])
+    
+  def test_send_message2(self):
+    send_message("user1", "user3", "message1to3b", {}, test_messages)
+    self.assertEqual(test_messages["user3"]["user1"], ["message1to3a", "message1to3b"])
 
 if __name__ == '__main__':
-  print("Unit tests running...")
+  print("Unit tests running!")
   unittest.main()
-  print("Unit tests completed!")
