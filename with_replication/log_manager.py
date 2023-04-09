7951002505d33b5b8ef6e8f1426074b02a846a0d
catyeo18 <catherineyeo2018@gmail.com>
2023-04-09 17:26:36 -0400

    file opening and closing

--- with_replication/log_manager.py
+++ with_replication/log_manager.py
@@ -4,9 +4,10 @@ import threading
 
 
 class LogManager:
-  def __init__(self, ip, port):
+  def __init__(self, ip, port, server_name):
     self.ip = ip
     self.port = port
+    self.server_name = server_name
     self.log = []
 
     self.current_term = 0
@@ -18,5 +19,26 @@ class LogManager:
     self.client_threads = {}
   
   def write_to_logs(self):
+    # TODO: ADD LOGIC OF WHAT DATA TO WRITE
+    data = ""
+    
+    log_filepath = "logs/" + self.server_name + ".txt"
+    file = open(log_filepath, "a+")
+    file.write(data + "\n")
+    file.close()
 
-  def restore_from_logs(self, log_filepath):
+    return data
+
+  def restore_from_logs(self):
+    log_filepath = "logs/" + self.server_name + ".txt"
+
+    if not os.path.exists(log_filepath):
+      print("Error: log filepath does not exist")
+    
+    file = open(log_filepath, "r+")
+    all_lines = file.read().splitlines()
+    non_empty_lines = list(filter(lambda x: x != '', all_lines))
+    file.close()
+
+    # TODO: RESTORE LOGIC
+      
