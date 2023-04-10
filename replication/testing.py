c3f0e4a82d08d485179ca58b94654775f19b61d1
catyeo18 <catherineyeo2018@gmail.com>
2023-04-10 12:23:53 -0400

    Unit tests for server components

--- replication/testing.py
+++ replication/testing.py
@@ -1,37 +1,58 @@
 import unittest
 from unittest.mock import MagicMock, Mock, patch
 import time
-import socket
-import threading
-from _thread import *
 
-from server import list_accounts, login
-import server, client
+import server
+from server import list_accounts, create_account, update_data
 
 
 # Overwrite mock methods from unittest.mock
-class MockSocket:
-
-class MockThread:
-  def __init__(self, socket):
-    self.socket = socket
-    # self.host = host
-    # self.port = port
+# class MockThread:
+#   def __init__(self, socket):
+#     self.socket = socket
+#     # self.host = host
+#     # self.port = port
   
-  def send(self, message):
-    self.socket = message
-    print("Sending message:", message)
+#   def send(self, message):
+#     self.socket = message
+#     print("Sending message:", message)
 
-  def receive(self, message):
-    self.socket = None
+#   def receive(self, message):
+#     self.socket = None
 
-  def start():
-    pass
+#   def start():
+#     pass
 
-  def join():
-    pass
+#   def join():
+#     pass
+
 
 class UnitTests(unittest.TestCase):
 
-  def test_leader_server(self):
+  # Tests: create_account, login, and update_data
+  def test_create_account(self):
+    server.server_id = 0
+    create_account("user1")
+    self.assertEqual(server.messages["user1"], [])
+    create_account("user2")
+    self.assertEqual(server.messages["user2"], [])
+    create_account("user3")
+    self.assertEqual(server.messages["user3"], [])
+
+  # Tests list_accounts logic
+  def test_list_accounts(self):
+    self.assertEqual(set(list_accounts("")), set(["user1", "user2", "user3"]))
+    self.assertEqual(set(list_accounts("^use")), set(["user1", "user2", "user3"]))
+    self.assertEqual(set(list_accounts("^user[13]$")), set(["user1", "user3"]))
+    self.assertEqual(list_accounts(".*1.*"), ["user1"])
+    self.assertEqual(list_accounts(".*[4567890].*"), [])
     
+  # Tests start_heartbeat and send_heartbeat processes
+  @patch('server.start_heartbeat')
+  def test_start_heartbeat(self, mock_start_heartbeat):
+    server.start_heartbeat(0)
+    mock_start_heartbeat.assert_called()
+
+if __name__ == '__main__':
+  print("Unit tests running!")
+  unittest.main()
