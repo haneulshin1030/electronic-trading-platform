fc9f4088c75eb4a22c64a3767c2cec524690a1ec
Haneul Shin <haneulshin1030@gmail.com>
2023-04-30 02:42:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -16,6 +16,8 @@ from concurrent import futures
 HOST = '127.0.0.1'
 PORT = 8000
 
+########## SERVER INFORMATION ##########
+
 # Current threads that are running.
 threads = []
 
@@ -25,7 +27,7 @@ event = threading.Event()
 # Current server.
 server = None
 
-# Record mapping index -> replica.
+# Dictionary mapping index -> replica.
 address_list = [f"{HOST}:{PORT}", f"{HOST}:{PORT + 1}", f"{HOST}:{PORT+ 2}"]
 
 # Server id of leader.
@@ -34,39 +36,112 @@ leader = None
 # Current live servers.
 active_servers = [None, None, None]
 
-# List of currently active users
-user_list = []
+########## MARKET INFORMATION ##########
 
-# Maximum message size
-MAX_MESSAGE_SIZE = 280
+# List of universe of symbols
+symbol_list = ['AAPL', 'TSLA', 'USD']
 
-# Record mapping username -> boolLoggedIn, where boolLoggedIn is True if and only if username is logged in
-logged_in = {}
+# Default dictionary of positions in symbols
 
-# Record mapping receive_username -> [message_1, ..., message_n], where message_i is a string of the form "From {user}: {message}"
+zero_positions = dict.fromkeys(symbol_list, 0)
+
+dir_list = ['buy', 'sell']
+
+# Dictionary mapping symbol -> {buy -> bid dictionary, sell -> offer dictionary}
+# Where the bid and offer dictionaries map price -> [{user_1, quantity_1}, ..., {user_n, quantity_n}]
+# sorted by time priority, such that if i < j, user i has time priority over user j
+open_orders = dict.fromkeys(symbol_list, {'buy': {}, 'sell': {}})
+
+# Dictionary mapping symbol -> {buy -> bid dictionary, sell -> offer dictionary}
+# Where the bid and offer dictionaries map price -> total quantity
+order_book = dict.fromkeys(symbol_list, {'buy': {}, 'sell': {}})
+
+########## USER INFORMATION ##########
+
+# Dictionary mapping user -> online status (bool)
+user_status = {}
+
+# Dictionary mapping user -> password
+passwords = {}
+
+# Dictionary mapping user, symbol -> user's position in that symbol in shares
+positions = {}
+
+# Dictionary mapping user -> [message_1, ..., message_n], where message_i is a response message from the server
 messages = {}
 
 # Error string for if the server is not the leader.
 ERROR_NOT_LEADER = "Error: server is not the leader."
 
-def store_messages(messages, file_name):
+def save_data(open_orders, order_book, user_status, passwords, positions, messages, file_name):
     """
-    Store the current dictionary of pending messages in a CSV file.
+    Store the current dictionary of the order book, user online status, user passwords, user positions, and user messages.
     """
-    with open(f"{file_name}.csv", "w+") as f:
+
+    # Store open orders
+    with open(f"open_orders_{file_name}.csv", "w+") as f:
         csv_writer = csv.writer(f)
 
-        # Store user list and message queue
-        for username, message_list in list(messages.items()):
-          csv_writer.writerow(["users_list"] + user_list)
+        for symbol in symbol_list:
+          for dir in dir_list:
+            for price in open_orders[symbol][dir]:
+              for user, size in open_orders[symbol][dir][price]:
+                csv_writer.writerow([symbol] + [dir] + [price] + [user] + [size])
+              
+    # Store order book
+    with open(f"order_book_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for symbol in symbol_list:
+          for dir in dir_list:
+            for price, size in list(order_book[symbol][dir].items()):
+              csv_writer.writerow([symbol] + [dir] + [price] + [size])
+                
+    # Store user status
+    with open(f"user_status_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user, online in list(user_status.items()):
+          csv_writer.writerow([user] + [online])
+
+    # Store user status
+    with open(f"passwords_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user, password in list(passwords.items()):
+          csv_writer.writerow([user] + [password])
+
+    # Store positions
+    with open(f"positions_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user in positions.keys():
+          for symbol, position in list(positions[user].items()):
+            csv_writer.writerow([user] + [symbol] + [position])
+
+    # Store message queue
+    with open(f"messages_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user, message_list in list(messages.items()):
           for message in message_list:
-            csv_writer.writerow([username] + [message])
+            csv_writer.writerow([user] + [message])
+
+def update_order_book(symbol, dir, price, user, size):
+  global order_book
+  
+  symbol, dir, price, user, size = row
+  if price in open_orders[symbol][dir]:
+    open_orders[symbol][dir][price].append({user, size})
+  else:
+    open_orders[symbol][dir][price] = ({user, size})
+  
 
 def update_data():
     """
-    Sends the updated data, including the list of pending messages and the active users, to each of the servers.
+    Sends the current dictionary of the order book, user online status, user passwords, user positions, and user messages to each of the servers.
     """
-    global server_id
+    global open_orders, order_book, user_status, passwords, positions, messages, server_id
     if leader != server_id:
         return
     for id, alive in enumerate(active_servers):
@@ -74,8 +149,8 @@ def update_data():
             channel = grpc.insecure_channel(address_list[id])
             stub = pb2_grpc.ChatStub(channel)
             try:
-                store_messages(messages, server_id)
-                stub.Send(pb2.Data(csv=json.dumps(messages), user_list=json.dumps(user_list)))
+                save_data(open_orders, order_book, user_status, passwords, positions, messages, file_name):
+                stub.Send(pb2.ServerData(open_orders=json.dumps(open_orders), order_book=json.dumps(order_book), user_status=json.dumps(user_status), passwords=json.dumps(passwords), positions=json.dumps(positions), messages=json.dumps(messages)))
             except:
                 print(f"Server failure: {id}")
                 active_servers[id] = False
@@ -84,103 +159,98 @@ class ChatServicer(pb2_grpc.ChatServicer):
     def Heartbeat(self, request, context):
         return pb2.HeartbeatResponse(leader=leader)
     
-    def Send(self, request, context):
+    def Send(self, order, context):
         """
         Send updated data to the databases.
         """
-        global messages, user_list, server_id
-        messages = json.loads(request.csv)
-        user_list = json.loads(request.user_list)
+        global open_orders, order_book, user_status, passwords, positions, messages
+        open_orders = json.loads(order.open_orders)
+        order_book = json.loads(order.order_book)
+        user_status = json.loads(order.user_status)
+        passwords = json.loads(order.passwords)
+        positions = json.loads(order.positions)
+        messages = json.loads(order.messages)
         
-        store_messages(messages, server_id)
+        save_data(open_orders, order_book, user_status, passwords, positions, messages, file_name):
         return pb2.UserResponse()
     
-    def ServerResponse(self, request, context): 
+    def ServerResponse(self, order, context): 
         '''
         Manage the server's response to user input.
         '''
-        return handle_server_response(request.opcode, request.username, request.recipient, request.message, request.regex)
+        return handle_server_response(order.opcode, order.user, order.password, order.dir, order.symbol, order.price, order.size)
     
-    def ClientMessages(self, username, context):
+    def ClientMessages(self, user, context):
         '''
         Return the client's pending messages, and update the data accordingly.
         '''
-        username = username.username
+        user = user.username
         
-        if username not in user_list:
-            user_list.append(username)
-            update_data()
+        # Shouldn't need this
+        # if user not in user_status:
+        #     user_status.append(username)
+        #     update_data()
 
-        print(user_list)
-        while username in user_list:
-            if username in messages and len(messages[username]) != 0:
-                for message in messages[username]:
-                  yield pb2.Response(response = message)
-                messages[username] = []
+        while user in messages.keys():
+              for message in messages[user]:
+                yield pb2.Response(response = message)
+              messages[user] = []
         update_data()
 
     def Leader(self, request, context):
         return pb2.LeaderResponse(leader = leader)
 
 
-def list_accounts(criteria):
-  """
-  List accounts (or a subset of the accounts, by text wildcard).
-  """
-  global user_list
-  unique_elements = list(set(user_list))
-  if criteria == "":
-    return unique_elements
-  else:
-    matching_names = []
-    regex = re.compile(criteria)
-    for key in unique_elements:
-      if regex.match(key):
-        matching_names.append(key)
-    return matching_names
-
-
 def login(username):
   """
   Log the user in.
   """
-  global user_list
-  user_list.append(username)
-  update_data()
-  return "Account " + username + " logged in."
+  global user_status
+  user_status[username] = True
+  return "Success: Account " + username + " logged in."
 
-def create_account(username):
+def create_account(username, password):
   """
-  Create new account.
+  Create new account and initialize user information.
   """
+
+  global user_status, passwords, positions, messages
+
+  user_status[username] = True
+  passwords[username] = password
+  positions[username] = zero_positions
   messages[username] = []
-  return login(username)
+  return "Success: Account " + username + " created and logged in."
 
-def handle_server_response(opcode, request_username, recipient, message, regex):
+def valid_password(password):
+  return True
+
+def trade_message(dir, symbol, price, size):
+  action = "Bought" if dir == "buy" else "Sold"
+  preposition = "for" if dir == "buy" else "at"
+  return f"{action} {size} shares of {symbol} {preposition} ${price}/share."
+
+def handle_server_response(opcode, username, password, dir, symbol, price, size):
     '''
     Handle the server's response to the input.
     '''
     # If this server is not the leader, error.
-    global leader
-    global server_id
-    global user_list
+    global leader, server_id
+    global user_status, passwords, positions, messages
     if server_id != leader:
-        return pb2.Response(response = ERROR_NOT_LEADER)
+      return pb2.Response(response = ERROR_NOT_LEADER)
 
-    username = None
+    current_user = None
     response = None
 
     # Create account.
     if opcode == "create": 
-      username = request_username
-
-      # Log out previous user.
-      if username and request_username != username:
-        logged_in[username] = False
 
       # Check whether username is unique.
-      if username in user_list:
+      if username in user_status:
         response = "Username already exists."
+      elif not valid_password(password):
+        response = "Invalid password."
       else:    
         # Create new user.
         response = create_account(username)
@@ -189,66 +259,91 @@ def handle_server_response(opcode, request_username, recipient, message, regex):
     # Log in.
     elif opcode == "login": 
       new_username = request_username
-
-      # Log out previous user.
-      if username and new_username != username:
-        logged_in[username] = False
-      username = new_username   
       
       # Check whether user exists.
-      if username not in user_list:
-        response = "Username does not exist."
+      if username not in user_status or passwords[username] != password:
+        response = "Username and/or password is not valid."
       else:
         response = login(username)
-
-    # Response to starting prompt.
-    elif opcode == "start": 
-      username = request_username
-      print(username)
-
-      # Check whether user exists.
-      if username not in user_list:
-        response = create_account(username)
-      else:
-        response = login(username)
-
-    # List accounts.
-    elif opcode == "list": 
-      criteria = regex
-      match_accounts = list_accounts(criteria)
-      response = "Accounts: " + str(match_accounts)
-    
-    # Send message to a user.
-    elif opcode == "send":
-      username = request_username
-      receive_user = recipient
-      # Check whether recipient exists.
-      if receive_user not in user_list:
-        response = "Username " + receive_user + " does not exist."
-      # Send message.
-      else:
-        msg = "From " + username + ": " + message
-        messages[receive_user].append(msg)
-        response = "Message sent to " + receive_user
         update_data()
-        # send_database_and_users()
 
-    # Delete account
-    elif opcode == "delete":
-      user_to_delete = recipient
-      # Unlike in the original version, we do not allow active clients to be logged out.
-      if user_to_delete in user_list:
-         response = "Cannot delete a user who is currently active."
-         return pb2.Response(response = response) 
+    # Post a user's order in the market and execute any matched trades
+    # Think about race conditions? Lock and unlock?
+    elif opcode == "order":
       
-      if user_to_delete in user_list:
-        del messages[user_to_delete]
-        del user_list[user_to_delete]
-        response = "Account " + user_to_delete + " deleted."
-        update_data()
+      # Opposite direction
+      if dir == "buy":
+        opp = "sell"
+        sgn = 1
+        opp_sgn = -1
       else:
-        response = "Account " + user_to_delete + " doesn't exist."
-   
+        opp = "buy"
+        sgn = -1
+        opp_sgn = 1
+      
+      # Check if there are matching trades, and excute them if so
+      
+      trade_size = 0
+      cumulative_price = 0
+      
+      # Lock?
+      
+      while True:
+        # Find best existing bid or offer price
+        if opp == "sell":
+          best_price = min(open_orders[symbol][opp], key=open_orders[symbol][opp].get)
+          if price < best_price:
+            break
+        else:
+          best_price = min(open_orders[symbol][opp], key=open_orders[symbol][opp].get)
+          if price > best_price:
+            break
+        
+        while size - trade_size > 0 and len(open_orders[symbol][opp][best_price]) > 0:
+          cur_size = min(open_orders[symbol][opp][best_price][0][1], size - trade_size)
+          
+          # Trade occurred
+          if cur_size > 0:
+            trade_size += cur_size
+            cumulative_price += cur_size * best_price
+            counterparty = open_orders[symbol][opp][best_price][0][0]
+            
+            # Update counterparty information
+            positions[counterparty][symbol] += opp_sgn * cur_size
+            positions[counterparty]['USD'] += sgn * cur_size * best_price
+            messages[counterparty].append(trade_message(opp, symbol, best_price, cur_size))
+            
+            # Update open orders
+            if cur_size < open_orders[symbol][opp][best_price][0][1]:
+              open_orders[symbol][opp][best_price][0][1] -= cur_size
+            else:
+              open_orders[symbol][opp][best_price][0].pop(0)
+            
+            # Update order book
+            if cur_size == order_book[symbol][opp][best_price]:
+              order_book[symbol][opp].pop(best_price)
+            else:
+              order_book[symbol][opp][best_price] -= cur_size
+      
+      # Update user information
+      average_price = cumulative_price / trade_size
+      positions[counterparty][symbol] += sgn * trade_size
+      positions[counterparty]['USD'] += opp_sgn * trade_size * average_price
+      messages[counterparty].append(trade_message(dir, symbol, average_price, trade_size))       
+      
+      # Update open orders and order book
+      if trade_size < size:
+        if price in order_book[symbol][dir]:
+          open_orders[symbol][dir][price].append({username, size - trade_size})
+          order_book[symbol][dir][price] += size - trade_size
+        else:
+          open_orders[symbol][dir][price] = [{username, price}]
+          order_book[symbol][dir][price] = size - trade_size
+
+      update_data()
+      
+      # Unlock?
+      
     # Exception
     elif opcode == "except":
       del messages[request_username]
@@ -341,15 +436,74 @@ def start_server():
     server = grpc.server(concurrent.futures.ThreadPoolExecutor(max_workers=12))
     pb2_grpc.add_ChatServicer_to_server(ChatServicer(), server)
 
-    global messages
-    global user_list
-    # messages = {}
+    global open_orders, order_book, user_status, passwords, positions, messages
 
-    # Create csv file and append data.
-    open(f"{server_id}.csv", "a+")
-    with open(f"{server_id}.csv", "r+") as csv_file:
-        csv_reader = csv.reader(csv_file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    # ^ make sure these are initialized?
 
+    # Read csv files into the server data
+    
+    open(f"open_orders_{server_id}.csv", "a+")
+    with open(f"open_orders_{server_id}.csv", "r+") as csv_file:
+      csv_reader = csv.reader(csv_file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+      for row in csv_reader:
+        symbol, dir, price, user, size = row
+        if price in open_orders[symbol][dir]:
+          open_orders[symbol][dir][price].append({user, size})
+        else:
+          open_orders[symbol][dir][price] = ({user, size})
+        
+
+
+    # Store open orders
+    with open(f"open_orders_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for symbol in symbol_list:
+          for dir in dir_list:
+            for price in open_orders[symbol][dir]:
+              for user, size in open_orders[symbol][dir][price]:
+                csv_writer.writerow([symbol] + [dir] + [price] + [user] + [size])
+              
+    # Store order book
+    with open(f"order_book_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for symbol in symbol_list:
+          for dir in dir_list:
+            for price, size in list(order_book[symbol][dir].items()):
+              csv_writer.writerow([symbol] + [dir] + [price] + [size])
+                
+    # Store user status
+    with open(f"user_status_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user, online in list(user_status.items()):
+          csv_writer.writerow([user] + [online])
+
+    # Store user status
+    with open(f"passwords_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user, password in list(passwords.items()):
+          csv_writer.writerow([user] + [password])
+
+    # Store positions
+    with open(f"positions_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user in positions.keys():
+          for symbol, position in list(positions[user].items()):
+            csv_writer.writerow([user] + [symbol] + [position])
+
+    # Store message queue
+    with open(f"messages_{file_name}.csv", "w+") as f:
+        csv_writer = csv.writer(f)
+
+        for user, message_list in list(messages.items()):
+          for message in message_list:
+            csv_writer.writerow([user] + [message])
+            
+            
         for row in csv_reader:
             if row[0] == "users_list":
                user_list += row
