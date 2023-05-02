637881dccfcc52a3fea1f8f73c6008bb73a3e163
catyeo18 <catherineyeo2018@gmail.com>
2023-05-02 14:47:36 -0400

    Basic setup of all 3 tables set up, fixed display errors with optionmenu

--- frontend/customer_client.py
+++ frontend/customer_client.py
@@ -2,11 +2,10 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 
 class CustomerClient(tk.Frame):
-  def __init__(self, parent, stock_symbol, customer_id):
-    tk.Frame.__init__(self, parent)
-    
-    self.customer_id = customer_id
-    self.stock_symbol = stock_symbol
+  def __init__(self):
+    # tk.Frame.__init__(self)
+    # self.customer_id = customer_id
+    # self.stock_symbol = stock_symbol
 
     # Create root window: Customer Client inputs
     self.root = tk.Tk()
@@ -18,8 +17,7 @@ class CustomerClient(tk.Frame):
     self.symbol_input.grid(row=0, column=1, padx=5, pady=5, sticky="W")
     
     tk.Label(self.root, text="Transaction Type:").grid(row=1, column=0, padx=5, pady=5, sticky="W")
-    self.transaction_type = tk.StringVar()
-    self.transaction_type.set("BUY")
+    self.transaction_type = tk.StringVar(value="BUY")
     self.transaction_type_select = tk.OptionMenu(self.root, self.transaction_type, "BUY", "SELL")
     self.transaction_type_select.grid(row=1, column=1, padx=5, pady=5, sticky="W")
     
@@ -37,19 +35,41 @@ class CustomerClient(tk.Frame):
     # Create second window: Message Log
     self.window2 = tk.Toplevel(self.root)
     self.window2.title('Notification Log')
-    self.window2.geometry('400x200+0+300')
+    self.window2.geometry('400x200+0+350')
 
     # HARDCODE TEST
-    tk.Label(self.window2, text="Posted an order to buy 100 shares of AAPL for $170.01/share.")
+    tk.Label(self.window2, text="").grid(row=0)
+    tk.Label(self.window2, text="Posted an order to buy 100 shares of AAPL for $170.01/share.").grid(row=1)
 
     # Create third window: Positions Table
     self.window3 = tk.Toplevel(self.root)
     self.window3.title('Positions Table')
-    self.window3.geometry('400x200+0+500')
+    self.window3.geometry('400x200+0+650')
+
+    tk.Label(self.window3, text="").grid(row=0, columnspan=7)
+    ttk.Separator(self.window3, orient="horizontal").grid(row=1, column=0, columnspan=7, sticky="ew")
+    ttk.Separator(self.window3, orient="vertical").grid(row=2, column=0, rowspan=1, sticky="ns")
+    tk.Label(self.window3, text="Stock").grid(row=2, column=1, sticky="W")
+    ttk.Separator(self.window3, orient="vertical").grid(row=2, column=2, rowspan=1, sticky="ns")
+    tk.Label(self.window3, text="Price").grid(row=2, column=3, sticky="W")
+    ttk.Separator(self.window3, orient="vertical").grid(row=2, column=4, rowspan=1, sticky="ns")
+    tk.Label(self.window3, text="Quantity").grid(row=2, column=5, sticky="W")
+    ttk.Separator(self.window3, orient="vertical").grid(row=2, column=6, rowspan=1, sticky="ns")
+    ttk.Separator(self.window3, orient="horizontal").grid(row=3, column=0, columnspan=7, sticky="ew")
+
+    # HARDCODE TEST
+    ttk.Separator(self.window3, orient="vertical").grid(row=4, column=0, rowspan=1, sticky="ns")
+    tk.Label(self.window3, text="AAPL").grid(row=4, column=1, sticky="W")
+    ttk.Separator(self.window3, orient="vertical").grid(row=4, column=2, rowspan=1, sticky="ns")
+    tk.Label(self.window3, text="$40.62").grid(row=4, column=3, sticky="W")
+    ttk.Separator(self.window3, orient="vertical").grid(row=4, column=4, rowspan=1, sticky="ns")
+    tk.Label(self.window3, text="50").grid(row=4, column=5, sticky="W")
+    ttk.Separator(self.window3, orient="vertical").grid(row=4, column=6, rowspan=1, sticky="ns")
+    ttk.Separator(self.window3, orient="horizontal").grid(row=5, column=0, columnspan=7, sticky="ew")
 
 
+    # Start the mainloop
+    self.root.mainloop()
 
 # For temporary testing
-root = tk.Tk()
-client = CustomerClient(root, "AAPL", "Cat").pack()
-root.mainloop()
+CustomerClient()
