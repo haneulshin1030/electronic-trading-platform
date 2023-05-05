cd7c5468cc41b42d49b5b406ba3c8f8ea71cc363
Haneul Shin <haneulshin1030@gmail.com>
2023-05-05 01:03:08 -0400

    valid password verification

--- server.py
+++ server.py
@@ -325,7 +325,21 @@ def create_account(username, password):
 
 
 def valid_password(password):
-    return True
+    l, u, p, d = 0, 0, 0, 0
+    if len(password) < 8:
+        return False
+    for i in password:
+        if i.islower():
+            l += 1
+        if i.isupper():
+            u += 1
+        if i.isdigit():
+            d += 1
+        if i == '@' or i == '$' or i == '_':
+            p += 1
+    if l >= 1 and u >= 1 and p >= 1 and d >= 1 and l + p + u + d == len(password):
+        return True
+    return False
 
 
 def trade_message(username, dir, symbol, price, size):
