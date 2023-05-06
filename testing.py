6c3b22241a8e8d6770aeda74b9874d423baaa9bd
catyeo18 <catherineyeo2018@gmail.com>
2023-05-05 23:31:54 -0400

    More trade matching unit tests to test edge cases and price-time priority

--- testing.py
+++ testing.py
@@ -8,29 +8,34 @@ from server import create_account, valid_password, login, post_order, handle_ser
 import client
 
 
-class UnitTests(unittest.TestCase):
+user1_pw = "P@33word"
+user2_pw = "pa$$w0rD"
+user3_pw = "p_SSw2rd"
 
+class UnitTests(unittest.TestCase):
+  
   # Tests account creation
   def test_create_account(self):
     server.server_id = 0
-    create_account("user1", "P@33word")
+
+    create_account("user1", user1_pw)
     self.assertEqual(server.messages["user1"], [])
-    self.assertEqual(server.passwords["user1"], "P@33word")
+    self.assertEqual(server.passwords["user1"], user1_pw)
     self.assertEqual(server.user_status["user1"], True)
-    create_account("user2", "pa$$w0rD")
+    create_account("user2", user2_pw)
     self.assertEqual(server.messages["user2"], [])
-    self.assertEqual(server.passwords["user2"], "pa$$w0rD")
+    self.assertEqual(server.passwords["user2"], user2_pw)
     self.assertEqual(server.user_status["user2"], True)
 
   # Tests login
   def test_login(self):
-    self.assertEqual(login("user1", "P@33word"), "Success: Account user1 logged in.")
+    self.assertEqual(login("user1", user1_pw), "Success: Account user1 logged in.")
     self.assertEqual(login("user1", "password"), "Failure: Username and/or password is not valid.")
-    self.assertEqual(login("user2", "P@33word"), "Failure: Username and/or password is not valid.")
+    self.assertEqual(login("user2", user1_pw), "Failure: Username and/or password is not valid.")
 
   # Tests password validity
   def test_valid_password(self):
-    self.assertEqual(valid_password("P@33word"), True)
+    self.assertEqual(valid_password(user1_pw), True)
 
     # Fails length
     self.assertEqual(valid_password("1234567"), False)
@@ -70,22 +75,54 @@ class UnitTests(unittest.TestCase):
     self.assertEqual(init_positions, server.positions)
 
   def test_match_trade(self):
+    """
+    These unit tests will test for a lot of scenarios, edge cases, 
+    price-time priority, and other financial metrics.
+    """
     # Reset params
     server.leader = 0
     server.order_book = {symbol: {"buy": SortedDict(), "sell": SortedDict()} for symbol in server.symbol_list}
     server.open_orders = {symbol: {"buy": SortedDict(), "sell": SortedDict()} for symbol in server.symbol_list}
 
     # User A: BUY, AAPL, $170, 100 shares -> no trade
-    handle_server_response('buy', 'user1', 'P@33word', 'buy', 'AAPL', 170, 100)
+    handle_server_response('buy', 'user1', user1_pw, 'buy', 'AAPL', 170, 100)
     self.assertEqual(server.order_book['AAPL']['buy'], SortedDict({170: 100}))
 
     # User B: SELL, AAPL, $175, 50 shares -> no trade
-    handle_server_response('sell', 'user2', 'pa$$w0rD', 'sell', 'AAPL', 175, 50)
+    handle_server_response('sell', 'user2', user2_pw, 'sell', 'AAPL', 175, 50)
     self.assertEqual(server.order_book['AAPL']['sell'], SortedDict({175: 50}))
 
     # User A: BUY, AAPL, $176, 120 -> TRADE happens
-    handle_server_response('buy', 'user1', 'P@33word', 'buy', 'AAPL', 176, 20)
-    self.assertEqual(server.order_book['AAPL']['sell'], SortedDict({175: 30}))
+    handle_server_response('buy', 'user1', user1_pw, 'buy', 'AAPL', 176, 120)
+    self.assertEqual(server.order_book['AAPL']['buy'], SortedDict({170: 100, 176: 70}))
+
+    # User B: BUY, TSLA, $160, 100 -> no trade
+    handle_server_response('buy', 'user1', user1_pw, 'buy', 'TSLA', 160, 100)
+    self.assertEqual(server.order_book['TSLA']['buy'], SortedDict({160: 100}))
+
+    # User B: SELL, AAPL, $168, 200 -> TRADE happens
+    # tests PRICE-time priority
+    handle_server_response('sell', 'user2', user2_pw, 'sell', 'AAPL', 168, 200)
+    self.assertEqual(server.order_book['AAPL']['sell'], SortedDict({168: 30}))
+    
+    # User C: SELL, AAPL, $168, 142 -> no trade
+    create_account("user3", user3_pw)
+    handle_server_response('sell', 'user3', user3_pw, 'sell', 'AAPL', 168, 142)
+    self.assertEqual(server.order_book['AAPL']['sell'], SortedDict({168: 172}))
+
+    # User A: BUY, AAPL, $170, 200 -> TRADE happens
+    # tests price-TIME priority
+    handle_server_response('buy', 'user1', user1_pw, 'buy', 'AAPL', 170, 200)
+    self.assertEqual(server.order_book['AAPL']['buy'], SortedDict({170: 28}))
+
+    # User B: BUY, AAPL, $171, 84 -> no trade
+    handle_server_response('buy', 'user2', user2_pw, 'buy', 'AAPL', 171, 84)
+    self.assertEqual(server.order_book['AAPL']['buy'], SortedDict({170: 28, 171: 84}))
+
+    # User C: SELL, AAPL, $169, 300 -> TRADE happens
+    # tests RegNMS and average pricing
+    handle_server_response('sell', 'user3', user3_pw, 'sell', 'AAPL', 169, 300)
+    self.assertEqual(server.order_book['AAPL']['sell'], SortedDict({169: 188}))
 
   # Tests start_heartbeat and send_heartbeat processes
   @patch('server.start_heartbeat')
